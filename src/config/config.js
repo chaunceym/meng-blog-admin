f7c3be83e156ce566a54cb27e24a80956b58a733
chauncey <18322276773@163.com>
2020-07-01 11:31:21 +0800

    完成远程端的修改

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const adminUrl = 'http://127.0.0.1:7001/admin/'
+const adminUrl = 'http://121.199.1.64:7001/admin/'
 const servicePath = {
   checkLogin: `${adminUrl}checkLogin`,
   getTypeInfo: `${adminUrl}getTypeInfo`,
